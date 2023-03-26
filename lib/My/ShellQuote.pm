ebb2e232bf51d1c715dc799649579a4482108a6d
Darren Embry <dse@webonastick.com>
2023-03-26 18:10:23 -0400

    shell_quote module

---
+++ lib/My/ShellQuote.pm
@@ -0,0 +1,19 @@
+package My::DupeGroup;
+use warnings;
+use strict;
+use base 'Exporter';
+our @EXPORT = qw();
+our @EXPORT_OK = qw(shell_quote);
+sub shell_quote {
+    my ($string) = @_;
+    for ($string) {
+        # stolen from String::ShellQuote to reduce dependencies
+        s/'/'\\''/g;
+        s|((?:'\\''){2,})|q{'"} . (q{'} x (length($1) / 4)) . q{"'}|ge;
+        $_ = "'$_'";
+        s/^''//;
+        s/''$//;
+    }
+    return $string;
+}
+1;
