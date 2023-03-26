a742a9bcb1354e43e96d71a2ddb4713949882e64
Darren Embry <dse@webonastick.com>
2023-03-26 18:36:33 -0400

    object-orientify the progress.

--- lib/My/Progress.pm
+++ lib/My/Progress.pm
@@ -2,61 +2,89 @@ package My::Progress;
 ## no critic (ProhibitInteractiveTest)
 use warnings;
 use strict;
+
 use Time::HiRes qw(gettimeofday);
-our $counter = 0;
-our $total;
-our $counter2;
-our $total2;
-our $cols;
-our $enabled;
-our $modulo = 173;
-our $secs = 0.1;
-use base 'Exporter';
-our @EXPORT = qw();
-our @EXPORT_OK = qw(incr_progress cols_progress clear_progress);
-my $last;
-my $now;
-sub incr_progress {
-    return if !-t 2 || !$enabled;
-    ++$counter;
-    $now = gettimeofday();
-    return if defined $last && $now < ($last + $secs);
-    $last = $now;
-    my $line1 = "$counter";
-    $line1 .= "/$total" if defined $total;
-    $line1 .= " $counter2" if defined $counter2;
-    $line1 .= "/$total2" if defined $total2;
+
+our $COLS;
+
+sub new {
+    my ($class, %args) = @_;
+    my $self = bless(\%args, $class);
+    $self->{secs} //= 0.1;
+    $self->{cols} //= ($COLS //= compute_cols());
+    return $self;
+}
+
+sub incr {
+    return if !-t 2;
+    my ($self) = @_;
+    return if !$self->{enabled};
+
+    $self->{counter} //= 0;
+    ++$self->{counter};
+
+    my $now = gettimeofday();
+    return if defined $self->{last} && $now < ($self->{last} + $self->{secs});
+    $self->{last} = $now;
+
+    my $line1 = '' . $self->{counter};
+    $line1 .= "/" . $self->{total}    if defined $self->{total};
+    $line1 .= " " . $self->{counter2} if defined $self->{counter2};
+    $line1 .= "/" . $self->{total2}   if defined $self->{total2};
     if (!scalar @_) {
         return p("\r${line1}\e[K");
     }
     $line1 .= " ";
     my $str = shift;
     my $line2 = scalar @_ ? sprintf($str, @_) : $str;
-    if (defined $cols) {
-        my $pos = -($cols - length($line1) - 3);
+    if (defined $self->{cols}) {
+        my $pos = -($self->{cols} - length($line1) - 3);
         $line2 = substr($line2, $pos);
     }
     return p("\r${line1}${line2}\e[K");
 }
-sub clear_progress {
-    return if !-t 2 || !$enabled;
+
+sub incr2 {
+    return if !-t 2;
+    my ($self) = @_;
+    return if !$self->{enabled};
+
+    $self->{counter2} //= 0;
+    ++$self->{counter2};
+}
+
+sub clear {
+    return if !-t 2;
+    my ($self) = @_;
+    return if !$self->{enabled};
     p("\r\e[K");
 }
+
+sub DESTROY {
+    return if !-t 2;
+    my ($self) = @_;
+    return if !$self->{enabled};
+    $self->clear();
+}
+
 sub p {
-    return if !-t 2 || !$enabled;
     my $af = STDERR->autoflush(1);
     my $str = shift;
     print STDERR (scalar @_ ? sprintf($str, @_) : $str);
     STDERR->autoflush($af);
 }
-sub cols_progress {
-    return if !-t 2 || defined $cols;
+
+sub compute_cols {
+    return if !-t 2;
     my $size = `stty size`;
     return if !defined $size;
-    (undef, $cols) = split(' ', $size);
+    my (undef, $cols) = split(' ', $size);
     $cols -= 1 if defined $cols;
+    return $cols;
 }
+
 END {
     clear_progress();
 }
+
 1;
